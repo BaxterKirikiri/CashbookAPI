73d23d68a5f6afb280ba0ae6f115702ed7ee5082
Baxter Kirikiri <baxter.kirikiri@gmail.com>
2021-04-28 18:08:26 +1200

    Initial commit
    Based on default REST API code from visual studio. Currently implements bare minimum CRUD functionality.

---
+++ Dummy.API/Controllers/TransactionsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dummy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private static List<Transaction> Transactions = new List<Transaction> { new Transaction(1, 200), new Transaction(2, 400) };
+
+        [HttpGet]
+        public List<Transaction> GetAll()
+        {
+            return Transactions;
+        }
+
+        [HttpGet("{id}")]
+        public Transaction GetOne(int id)
+        {
+            return Transactions.Single(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Transaction transaction)
+        {
+            Transactions.Add(transaction);
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            Transactions = Transactions.Where(x => x.Id != id).ToList();
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, [FromBody] Transaction transaction)
+        {
+            Transaction t = Transactions.Single(x => x.Id == id);
+            t.Sum = transaction.Sum;
+        }
+    }
+}
---
+++ Dummy.API/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dummy.API
+{
+    public class Transaction
+    {
+        public Transaction(int id, int sum)
+        {
+            Id = id;
+            Sum = sum;
+        }
+        public int Id { get; set; }
+        public int Sum { get; set; }
+    }
+}
