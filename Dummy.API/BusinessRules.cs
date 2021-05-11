db1a6c579bbb8fe592df381eb3832ab960376e7d
Baxter Kirikiri <baxter.kirikiri@gmail.com>
2021-05-11 20:35:08 +1200

    Added BusinessRules.cs. Moved IsTaxYear method to BusinessRules.

---
+++ Dummy.API/BusinessRules.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dummy.API
+{
+    public static class BusinessRules
+    {
+        public static bool IsTaxYear(DateTime date, int year)
+        {
+            var startDate = new DateTime(year - 1, 4, 1);
+            var endDate = new DateTime(year, 3, 31);
+            return date >= startDate && date <= endDate;
+        }
+    }
+}
--- Dummy.API/Controllers/YearsController.cs
+++ Dummy.API/Controllers/YearsController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,23 +8,16 @@ namespace Dummy.API.Controllers
     [ApiController]
     public class YearsController : ControllerBase
     {
-        private bool IsTaxYear(DateTime date, int year)
-        {
-            var startDate = new DateTime(year - 1, 4, 1);
-            var endDate = new DateTime(year, 3, 31);
-            return date >= startDate && date <= endDate;
-        }
-
         [HttpGet("{year}/incomes")]
         public List<Income> GetIncomeByYear(int year)
         {
-            return Data.Incomes.Where(x => IsTaxYear(x.InvoiceDate, year)).ToList();
+            return Data.Incomes.Where(x => BusinessRules.IsTaxYear(x.InvoiceDate, year)).ToList();
         }
 
         [HttpGet("{year}/expenses")]
         public List<Expense> GetExpensesByYear(int year)
         {
-            return Data.Expenses.Where(x => IsTaxYear(x.InvoiceDate, year)).ToList();
+            return Data.Expenses.Where(x => BusinessRules.IsTaxYear(x.InvoiceDate, year)).ToList();
         }
     }
 }
